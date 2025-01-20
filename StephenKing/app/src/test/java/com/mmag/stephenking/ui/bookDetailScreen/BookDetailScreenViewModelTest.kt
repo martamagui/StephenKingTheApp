9555441eb94e3c711fc8b16b7591c27b3932164c
martamagui <73697174+martamagui@users.noreply.github.com>
2025-01-20 23:49:09 +0100

    Added more test in BookDetailScreenViewModelTest

--- StephenKing/app/src/test/java/com/mmag/stephenking/ui/bookDetailScreen/BookDetailScreenViewModelTest.kt
+++ StephenKing/app/src/test/java/com/mmag/stephenking/ui/bookDetailScreen/BookDetailScreenViewModelTest.kt
@@ -5,6 +5,7 @@ import com.mmag.stephenking.domain.model.StephenKingResponse
 import com.mmag.stephenking.domain.useCases.GetBookDetailUseCase
 import com.mmag.stephenking.ui.screens.bookDetailScreen.BookDetailScreenViewModel
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -35,6 +36,42 @@ class BookDetailScreenViewModelTest {
         viewModel.getBookDetail(bookId.toString())
 
         verify(getBookDetailUseCase).invoke(bookId.toString())
+        runCurrent()
+        assertEquals(response, viewModel.bookDetailScreenState.value)
     }
 
+    @Test
+    fun `getBookDetail should emit Loading and Success states when use case succeeds`() = runTest {
+        val bookId = 1
+        val book: Book = mock()
+        `when`(book.id).thenReturn(bookId)
+        val response = StephenKingResponse.Success(book)
+        val flow = flowOf(response)
+        `when`(getBookDetailUseCase.invoke(bookId.toString())).thenReturn(flow)
+
+        assertEquals(viewModel.bookDetailScreenState.value is StephenKingResponse.Loading, true)
+
+        viewModel.getBookDetail(bookId.toString())
+
+        verify(getBookDetailUseCase).invoke(bookId.toString())
+        runCurrent()
+        assertEquals(response, viewModel.bookDetailScreenState.value)
+    }
+
+
+    @Test
+    fun `getBookDetail should emit Loading and Error states when use case fails`() = runTest {
+        val id = "2"
+        val response: StephenKingResponse<Book> = StephenKingResponse.Error("Error", null)
+        val flow = flowOf(response)
+        `when`(getBookDetailUseCase.invoke(id)).thenReturn(flow)
+
+        assertEquals(viewModel.bookDetailScreenState.value is StephenKingResponse.Loading, true)
+
+        viewModel.getBookDetail(id)
+
+        verify(getBookDetailUseCase).invoke(id)
+        runCurrent()
+        assertEquals(response, viewModel.bookDetailScreenState.value)
+    }
 }
